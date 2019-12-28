7fe692ae03e5599f63082b325a66624eadd9b90b
Stan Biryukov <stanley.biryukov@understoryweather.com>
2019-12-27 21:44:26 -0600

    r string exclude columns fix

--- pyGLMM/_utils.py
+++ pyGLMM/_utils.py
@@ -6,11 +6,12 @@ def r_formula(df, dependent_var, re=None, excluded_cols=None):
     """
     df_columns = list(df.columns.values)
     df_columns.remove(dependent_var)
-    for col in excluded_cols:
-        try:
-            df_columns.remove(col)
-        except ValueError:
-            pass
+    if excluded_cols is not None:
+        for col in excluded_cols:
+            try:
+                df_columns.remove(col)
+            except ValueError:
+                pass
     frm = dependent_var + " ~ " + " + ".join(df_columns)
     if re:
         frm = frm + " + " + re
